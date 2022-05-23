c249eab61bb45e0c4ec97049e7c91e554359a18e
hyunklee <lhk924@gmail.com>
2022-05-23 22:00:51 +0900

    99%

--- YUSAPI/src/main/java/com/ssafit/yus/config/WebConfig.java
+++ YUSAPI/src/main/java/com/ssafit/yus/config/WebConfig.java
@@ -24,10 +24,10 @@ public class WebConfig implements WebMvcConfigurer{
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/swagger-ui/**")
-				.addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+		registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+		registry.addResourceHandler("/file/**").addResourceLocations("classpath:/static/file/");
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
 	}
-	
 //	@Autowired
 //	private JWTInterceptor jwtInterceptor;
 //	
